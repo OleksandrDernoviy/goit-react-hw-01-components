f3cad882e5e3e5f8ad32d2327aad688d6a497f4e
Oleksandr Dernoviy <130732150+Aex-Derny@users.noreply.github.com>
2023-11-07 10:01:13 +0200

    updCss

--- src/components/Statistics/index.jsx
+++ src/components/Statistics/index.jsx
@@ -5,7 +5,6 @@ import { getRandomHexColor } from './labelRandomColor'
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {/* <h2 className={css.title}>{title}</h2> */}
       {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(el => (
